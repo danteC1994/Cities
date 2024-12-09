126a902670dd1b110e81243d3b1f014c825d2f12
dante canizo <dantecanizo@dantes-MacBook-Pro-2.local>
2024-12-09 12:05:12 -0300

    Create list view

--- Cities/CityList/CitiesListView.swift
+++ Cities/CityList/CitiesListView.swift
@@ -9,11 +9,28 @@ import SwiftUI
 
 struct CitiesListView: View {
     @StateObject var viewModel: CitiesListViewModel
+    @State private var searchText = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                ForEach(viewModel.cities.indices, id: \.self) { index in
+                    HStack {
+                        Text("\(viewModel.cities[index].name), \(viewModel.cities[index].country)")
+                            .padding()
+                        Spacer()
+                    }
+                    .background(index.isMultiple(of: 2) ? Color.clear : Color.gray.opacity(0.2))
+                    .listRowInsets(EdgeInsets())
+                    .frame(maxWidth: .infinity)
+                    .contentShape(Rectangle())
+                }
+            }
+            .listStyle(PlainListStyle())
+            .searchable(text: $searchText)
             .task {
                 await viewModel.fetchCities()
             }
+        }
     }
 }
 
