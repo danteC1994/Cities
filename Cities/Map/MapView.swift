ee2fa888f690b94141e512aef3b1a861bb411410
dante canizo <dantecanizo@dantes-MacBook-Pro-2.local>
2024-12-09 22:04:06 -0300

    Add simple map

---
+++ Cities/Map/MapView.swift
@@ -0,0 +1,47 @@
+//
+//  MapView.swift
+//  Cities
+//
+//  Created by dante canizo on 09/12/2024.
+//
+
+import MapKit
+import SwiftUI
+
+struct MapView: View {
+    var city: City?
+
+    var body: some View {
+        ZStack {
+            if let city = city {
+                let region = MKCoordinateRegion(
+                    center: .init(latitude: city.coordinates.lat, longitude: city.coordinates.lon),
+                    span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                )
+                Map(coordinateRegion: .constant(region), showsUserLocation: false, userTrackingMode: nil, annotationItems: [city]) { city in
+                    MapAnnotation(coordinate: .init(latitude: city.coordinates.lat, longitude: city.coordinates.lon)) {
+                        VStack {
+                            Image(systemName: "mappin")
+                                .resizable()
+                                .scaledToFit()
+                                .foregroundColor(.red)
+                                .frame(width: 30, height: 30)
+                            Text(city.name)
+                                .font(.caption)
+                                .foregroundColor(.black)
+                        }
+                    }
+                }
+                .edgesIgnoringSafeArea(.all)
+            } else {
+                Text("Select a city to view its location")
+                    .font(.largeTitle)
+                    .foregroundColor(.gray)
+            }
+        }
+    }
+}
+
+#Preview {
+    MapView(city: .init(id: 3, name: "Hurzuf", country: "UA", coordinates: .init(lon: 34.283333, lat: 44.549999)))
+}
